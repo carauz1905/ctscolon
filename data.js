62ea43f05e544642e17a6ca0ab0e27eeff0d269c
Carlos Arauz <arauz.carlos@hotmail.com>
2026-03-23 18:43:01 -0500

    Corrige estructura del tour Marzipano

---
+++ data.js
@@ -0,0 +1,502 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bodega-actual-cts",
+      "name": "Bodega Actual CTS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.392151209290912,
+        "pitch": -0.08650097842474125,
+        "fov": 0.8793928495885855
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.5583042583976479,
+          "pitch": -0.6014723050442168,
+          "title": "Antenas Aruba&nbsp;",
+          "text": "Sin Administración"
+        },
+        {
+          "yaw": -3.084357912679586,
+          "pitch": -0.05211877533574949,
+          "title": "Ubicación Cuarto IT - CTS",
+          "text": "Se queire salir con fibra desde este Gabinete hasta el gabitene nuevo que se va a colocar ebn la otra galera"
+        },
+        {
+          "yaw": 2.1353208109384116,
+          "pitch": -0.4187429454042242,
+          "title": "Bandejas",
+          "text": "La galera actual tiene el cableado por bandejas, aproximadamente a unos 12 metros de altura"
+        },
+        {
+          "yaw": -2.4304318116707186,
+          "pitch": -0.13849660701002975,
+          "title": "Cuarto IT Nueva Area",
+          "text": "en el 2do piso se encuentra el nuevo cuarto de IT para la extensión"
+        }
+      ]
+    },
+    {
+      "id": "1-frente-de-las-galeras",
+      "name": "Frente de las Galeras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.8511522163066072,
+        "pitch": -0.14017018989424201,
+        "fov": 0.8157327694949537
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.0450494038296885,
+          "pitch": -0.067917463722722,
+          "title": "Entrada CST",
+          "text": "La entrada de todo el personal a la nueva area se efectuara a traves de esta puerta"
+        },
+        {
+          "yaw": -0.6139396661959644,
+          "pitch": -0.05827037491806131,
+          "title": "puerta a area nueva",
+          "text": "Esta puerta se mantendra cerrada, por lo cual no se contempla control de acceso"
+        }
+      ]
+    },
+    {
+      "id": "2-zona-de-andenes",
+      "name": "Zona de Andenes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -1.1067129633143349,
+        "pitch": -0.15818579016909062,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.641869469936518,
+          "pitch": -0.005924622917838462,
+          "rotation": 0,
+          "target": "4-mitad-pasillo-1"
+        },
+        {
+          "yaw": -0.9380544979730523,
+          "pitch": -0.0052404080168741984,
+          "rotation": 0,
+          "target": "8-pasillo-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8975141223589329,
+          "pitch": -0.460085143003262,
+          "title": "2da Columna",
+          "text": "El espacio aprox entre columnas es de 9.8 mtrs.. el ancho de las dos columnas seria aprox 20 metros. se hará un cerramiento en este punto para dividir el area de la galera de Allep"
+        },
+        {
+          "yaw": 2.0618713080405655,
+          "pitch": 0.10001032518291453,
+          "title": "Puerta de acceso a oficina",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6325982098631133,
+          "pitch": -0.21303519499724644,
+          "title": "Pasillo 1",
+          "text": "la medida hasta el fondo es de aprox 100 metros"
+        },
+        {
+          "yaw": -1.3122411837961234,
+          "pitch": -0.20340364102299446,
+          "title": "Pasillo 2",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.9865587803014666,
+          "pitch": -0.1829193208553157,
+          "title": "Pasillo 3",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-andenes",
+      "name": "Andenes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 1.883109882184237,
+        "pitch": 0.07304269512000516,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.7748975514715761,
+          "pitch": 0.14561919545912616,
+          "title": "Andenes de Carga",
+          "text": "El area contara con 5 andenes"
+        }
+      ]
+    },
+    {
+      "id": "4-mitad-pasillo-1",
+      "name": "Mitad pasillo 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.285961146536666,
+        "pitch": -0.5210266949863502,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6565803355769724,
+          "pitch": -0.018639937936232087,
+          "rotation": 0,
+          "target": "2-zona-de-andenes"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5819611333445556,
+          "pitch": -1.5707963267948966,
+          "title": "Techo",
+          "text": "Vigas"
+        },
+        {
+          "yaw": -0.45725225907202827,
+          "pitch": 0.29842238108707697,
+          "title": "Mitad de pasillo 1",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-mezanine-oficinas",
+      "name": "Mezanine oficinas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 2.8333491268570548,
+        "pitch": 0.006265863315306319,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2581194672375098,
+          "pitch": -0.002059245382874053,
+          "rotation": 0,
+          "target": "6-cuarto-de-it-nueva-area"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0471035598129133,
+          "pitch": 0.11787377608435357,
+          "title": "Puerta a permanecer cerrada",
+          "text": "Se hara un acceso de la galera de CTS a esta Galera"
+        }
+      ]
+    },
+    {
+      "id": "6-cuarto-de-it-nueva-area",
+      "name": "Cuarto de IT nueva area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -0.35416899795145795,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.7427930900949171,
+          "pitch": -0.08159851435106802,
+          "title": "Gabinete actual",
+          "text": "Este gabinete sera removido, en esta area colocar el nuevo gabinete con swicht, nvr, ups, entre otros"
+        }
+      ]
+    },
+    {
+      "id": "7-techo-cuarto-it",
+      "name": "Techo cuarto IT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": 0.2364459447848244,
+        "pitch": 0.30814961238989724,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.538391712807611,
+          "pitch": -0.07654841698530745,
+          "rotation": 0,
+          "target": "2-zona-de-andenes"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1546104159653385,
+          "pitch": -0.04556047046381906,
+          "title": "Cuarto de IT CTS",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.31032570574998886,
+          "pitch": 0.30720520838987575,
+          "title": "Gabinete Nuevo",
+          "text": "Justo aqui debajo"
+        }
+      ]
+    },
+    {
+      "id": "8-pasillo-3",
+      "name": "Pasillo 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3880,
+      "initialViewParameters": {
+        "yaw": -2.206874986630181,
+        "pitch": -0.30803493345232624,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.999819875502781,
+          "pitch": -0.23762584313391066,
+          "rotation": 0,
+          "target": "7-techo-cuarto-it"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5603434297351875,
+          "pitch": -0.007536096671735493,
+          "title": "2da columna",
+          "text": "hasta aqui llegara el area nueva"
+        },
+        {
+          "yaw": 2.164147476933902,
+          "pitch": -0.18136904917620456,
+          "title": "area de mezzanine",
+          "text": "detras de esta pared se encuantran las oficinas"
+        },
+        {
+          "yaw": -1.6986991579895623,
+          "pitch": -0.1464193213280005,
+          "title": "2da columna",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "CTS - COLÓN - ALEPH",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
